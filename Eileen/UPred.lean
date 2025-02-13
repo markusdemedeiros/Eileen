aa199728b49deb9b783ff22f6d7980a2ee470d7a
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-02-13 10:50:36 -0500

    upred COFE

--- Eileen/UPred.lean
+++ Eileen/UPred.lean
@@ -9,7 +9,7 @@ TODO items:
 import Eileen.Cmra
 
 def UPred (M : Type) [UCMRA M] : Type :=
-  { pred : ℕ -> M -> Prop // ∀ n1 n2 x1 x2, pred n1 x1 -> x1 ≲[n1] x2 -> n1 ≤ n2 -> pred n2 x2 }
+  { pred : ℕ -> M -> Prop // ∀ n1 n2 x1 x2, pred n1 x1 -> x1 ≲[n2] x2 -> n2 ≤ n1 -> pred n2 x2 }
 
 namespace UPred
 
@@ -65,5 +65,49 @@ instance : OFE (UPred M) where
     · intro P _ _ _ _ _
       apply P
       trivial
+
+-- TODO: Cleanup
+instance : COFE (UPred M) where
+  lim c :=
+    Subtype.mk
+      (fun n x => ∀n', n' ≤ n -> ✓[n'] x -> (c n').val n' x)
+      (by
+        simp only []
+        intro n1 n2
+        intros x1 x2 H H1 H2 n' H3 H4
+        apply (c.car n').property
+        · apply H
+          · exact Nat.le_trans H3 H2
+          · apply CMRA.ivalid_iincluded_ivalid _ _ _ _ H4
+            apply CMRA.iincluded_le_mono _ _ _ _ _ H1
+            exact H3
+        · exact CMRA.iincluded_le_mono M n2 n' x1 x2 H1 H3
+        · apply le_refl)
+  completeness n c := by
+    intro i n' Hin Hv
+    simp []
+    rcases c with ⟨ c, Hc ⟩
+    have H : (c n').val n' i <-> (c n).val n' i := by
+      simp_all []
+      apply symm
+      apply (Hc Hin i n' _ Hv)
+      apply Nat.le_refl
+    apply (Iff.trans _ H)
+    apply Iff.intro
+    · intro H3 ; apply H3 <;> trivial
+    intro A B C D
+    simp []
+    apply (Hc C i _ _ D).mp
+    · apply (c n').property
+      · apply A
+      · apply CMRA.iincluded_le_mono _ n'
+        · unfold iincluded
+          exists 1
+          rw [MulOneClass.mul_one]
+          apply refl
+        · apply C
+      · apply C
+    · apply le_refl
+
 
 end UPred
